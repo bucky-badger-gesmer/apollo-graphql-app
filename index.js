6e0cedcb712c10ff3c4aa1de1948d8a3c5cfe96b
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-12 08:46:48 -0600

    chore: more init

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Define your GraphQL schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Start the server
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
